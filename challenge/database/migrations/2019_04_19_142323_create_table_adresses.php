aeda5e19c2c50aa9c128c6e1b56e8dbab249c88f
Rafael Ferreti <rafael.ferreti@apis3.com>
2019-04-19 15:27:08 -0300

    Arquivos do desafio

---
+++ challenge/database/migrations/2019_04_19_142323_create_table_adresses.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableAdresses extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('id_person');
+            $table->string('postal_code', 9);
+            $table->string('address', 9);
+            $table->string('number', 10);
+            $table->string('complement')->nullable();
+            $table->string('state', 20);
+            $table->string('country', 50);
+
+            $table->foreign('id_person')->references('id')->on('people');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}
